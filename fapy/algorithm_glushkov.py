e57354019c63d3a2e7a3501894fb5683dd50bae4
Cédric HT <nope-mcnopeface@asdf.org>
2019-11-19 12:38:17 +0100

    Documented module algorithm_glushkov

--- fapy/algorithm_glushkov.py
+++ fapy/algorithm_glushkov.py
@@ -1,17 +1,15 @@
-"""Glushkov's algorithm
+"""Implementation of Glushvok's algorithm
+
+See also:
+    `Wikipedia page <https://en.wikipedia.org/wiki/Glushkov%27s_construction_algorithm>`_
 """
 
 from typing import (
-    Set,
     Tuple
 )
 
 
-from fapy.common import (
-    Letter
-)
 from fapy.finite_automaton import (
-    empty_word_automaton,
     FiniteAutomaton
 )
 from fapy.regular_expression import (
@@ -22,9 +20,42 @@ from fapy.regular_expression import (
 def _linearize_regular_expression(
         regular_expression: RegularExpression,
         index: int = 0) -> Tuple[RegularExpression, int]:
-    """"Makes a regular expression linear by renaming all variables to unique
-    names.
+    """"Linearizes a regular expression by renaming all variables to unique
+    names
+
+    Example::
+
+        re, _ = _linearize_regular_expression(
+            parse_regular_expression('a a + b*')
+        )
+        print(str(re))
+
+    prints ``a0 a1 + b2*``. The first number to be used can be specified with
+    the optional ``index`` argument::
+
+        re, _ = _linearize_regular_expression(
+            parse_regular_expression('a a + b*', 4)
+        )
+        print(str(re))
+
+    prints ``a4 a5 + b6*``
+
+    Args:
+        regular_expression: The :class:`regular_expression.RegularExpression`
+            to linearize
+        index: The starting index. Defaults to :math:`0`.
+
+    Returns:
+        A tuple formed by
+
+        1. The linearized regular expression
+        2. The next available index
+
+    Raises:
+        NotImplementedError: If :attr:`RegularExpression.node_type` is
+            invalid
     """
+
     if regular_expression.node_type == 'CONCAT':
         left, index = _linearize_regular_expression(
             regular_expression.left,
@@ -35,8 +66,10 @@ def _linearize_regular_expression(
             index
         )
         return (RegularExpression('CONCAT', left=left, right=right), index)
+
     if regular_expression.node_type == 'EPSILON':
         return (RegularExpression('EPSILON'), index)
+
     if regular_expression.node_type == 'LETTER':
         return (
             RegularExpression(
@@ -45,6 +78,7 @@ def _linearize_regular_expression(
             ),
             index + 1
         )
+
     if regular_expression.node_type == 'PLUS':
         left, index = _linearize_regular_expression(
             regular_expression.left,
@@ -55,16 +89,20 @@ def _linearize_regular_expression(
             index
         )
         return (RegularExpression('PLUS', left=left, right=right), index)
+
     if regular_expression.node_type == 'STAR':
         inner, index = _linearize_regular_expression(
             regular_expression.inner,
             index
         )
         return (RegularExpression('STAR', inner=inner), index)
+
     raise NotImplementedError(f'Unknown node type {regular_expression.node_type}')
 
 
 def glushkov(regular_expression: RegularExpression) -> FiniteAutomaton:
+    """Implementation of Glushkov's algorithm
+    """
 
     linearized, _ = _linearize_regular_expression(regular_expression)
     q_init_result = '0'
